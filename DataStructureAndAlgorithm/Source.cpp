8a45349d41c77610623af56940c934eaee873db1
sameer arshad <arshadsameer820@gmail.com>
2024-09-01 19:44:13 +0500

    Add project files.

---
+++ DataStructureAndAlgorithm/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "arrayfile.h"
+
+using namespace std;
+
+int main()
+{
+
+	check();
+
+}
---
+++ DataStructureAndAlgorithm/arrayfile.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef ARRAYFILE_H
+#define ARRAYFILE_H
+
+void check();
+
+#endif
